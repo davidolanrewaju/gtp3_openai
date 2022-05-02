42a2fd120f1603a94bef3dca7abab5cb9cda34d0
David <davidolanrewaju538@gmail.com>
2022-05-02 22:58:54 +0100

    Navbar Styled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Blog, Header, Footer, Features, Possibility, WhatGTP } from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import "./navbar.css";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
   return (
-      <div>
-          Navbar
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
       </div>
-  )
-}
+      <div className="gpt3__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
