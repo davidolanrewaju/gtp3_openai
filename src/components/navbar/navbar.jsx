233ef9f24cc8c48db9985e15a0b7c91d3be8b621
David <davidolanrewaju538@gmail.com>
2022-05-05 10:10:03 +0100

    Header

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
@@ -8,13 +8,23 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   const Menu = () => (
     <>
-      <p><a href="#home">Home</a></p>
-      <p><a href="#wgpt3">What is GPT?</a></p>
-      <p><a href="#possibility">Open AI</a></p>
-      <p><a href="#features">Case Studies</a></p>
-      <p><a href="#blog">Library</a></p>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#wgpt3">What is GPT?</a>
+      </p>
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Library</a>
+      </p>
     </>
-  )
+  );
 
   return (
     <div className="gpt3__navbar">
